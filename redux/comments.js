14a1acb8e895ed8ab69f4a9b27afc81851a73109
shreeshamishra1 <shreeshamishra.sm@gmail.com>
2021-08-27 17:26:28 -0700

    my first commit

---
+++ redux/comments.js
@@ -0,0 +1,18 @@
+import * as ActionTypes from './ActionTypes';
+
+export const comments = (state = { errMess: null, comments: []}, action) => {
+    switch (action.type) {
+        case ActionTypes.ADD_COMMENTS:
+            return {...state, errMess: null, comments: action.payload};
+
+        case ActionTypes.COMMENTS_FAILED:
+            return {...state, errMess: action.payload};
+
+            case ActionTypes.ADD_COMMENT:
+                return {...state, comments: state.comments.concat(addedcomment)};
+        
+
+        default:
+            return state;
+    }
+};
